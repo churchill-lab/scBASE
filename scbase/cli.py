088f33a74da317dfbe84e705143b9fa0c678da98
KB CHOI <kb.choi@jax.org>
2018-09-17 16:51:52 -0400

    [kbchoi] Implemented subcommand 'collate'

--- scbase/cli.py
+++ scbase/cli.py
@@ -54,8 +54,8 @@ def run_mcmc(loomfile, model, g_start, g_end, verbose):
 @main.command()
 @click.argument('indir', metavar='indir', type=click.Path(exists=True, resolve_path=True))
 @click.argument('loomfile', metavar='loomfile', type=click.Path(exists=True, resolve_path=True, dir_okay=False))
-@click.option('-c', '--counts', metavar='filetype', flag_value='counts')
-@click.option('-p', '--params', metavar='filetype', flag_value='params')
+@click.option('--counts', metavar='filetype', flag_value='counts')
+@click.option('--params', metavar='filetype', flag_value='params')
 @click.option('-n', '--name', metavar='filename', default=None)
 @click.option('-v', '--verbose', count=True, help='\'-v\' is Level 1 and \'-vv\' is Level 2')
 def collate(loomfile, indir, filetype, filename, verbose):
@@ -65,6 +65,7 @@ def collate(loomfile, indir, filetype, filename, verbose):
     :param indir:
     :param filetype:
     :param filename:
+    :param verbose:
     :return:
     """
     utils.configure_logging(verbose)
