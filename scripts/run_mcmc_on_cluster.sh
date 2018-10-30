fe37891fb30c5184a5d7ade21a708ae93320f147
KB CHOI <kb.choi@jax.org>
2018-10-30 13:16:24 -0400

    [kbchoi] Fixing a bug in run-mcmc-from-npz

--- scripts/run_mcmc_on_cluster.sh
+++ scripts/run_mcmc_on_cluster.sh
@@ -4,7 +4,7 @@
 source activate scbase
 cd $PBS_O_WORKDIR
 
-scbase run_mcmc_from_npz -m ${ASE_MODEL} ${TGX_MODEL} \
+scbase run-mcmc-from-npz -m ${ASE_MODEL} ${TGX_MODEL} \
                          --hapcode ${MAT_HAPCODE} ${PAT_HAPCODE} \
                          -o ${OUTFILE} \
                          ${INFILE}
