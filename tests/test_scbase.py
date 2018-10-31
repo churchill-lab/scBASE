926e162512612a5d1195ef13a7d6495d6969bd7e
KB CHOI <kb.choi@jax.org>
2018-10-30 22:34:09 -0400

    [kbchoi] Setting up Travis CI

--- tests/test_scbase.py
+++ tests/test_scbase.py
@@ -28,7 +28,7 @@ class TestScbase(unittest.TestCase):
         runner = CliRunner()
         result = runner.invoke(cli.main)
         assert result.exit_code == 0
-        assert 'scbase.cli.main' in result.output
+        assert 'main' in result.output
         help_result = runner.invoke(cli.main, ['--help'])
         assert help_result.exit_code == 0
         assert '--help  Show this message and exit.' in help_result.output
