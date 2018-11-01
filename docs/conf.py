a87e75b3af07d4032649583fa6893d6b73b2e288
KB CHOI <kb.choi@jax.org>
2018-11-01 19:49:43 -0400

    [kbchoi] Fixed syntax error in docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -48,8 +48,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'scBASE'
-copyright = u"2018, Kwangbom "KB" Choi, Ph.D."
-author = u"Kwangbom "KB" Choi, Ph.D."
+copyright = u'2018, Kwangbom \"KB\" Choi, Ph.D.'
+author = u'Kwangbom \"KB\" Choi, Ph.D.'
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
